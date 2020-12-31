438da7ee148c4dcb77da51df1a2c0f2a331d2aa6
alexandrotrevino <alexandro.trevino@gmail.com>
2020-12-31 15:34:55 -0800

    adding scripts

--- get_data.sh
+++ get_data.sh
@@ -2,13 +2,13 @@ mkdir data_files
 mkdir data_files/tsv
 mkdir data_files/rds
 
-while IFS= read -r links; do
+while IFS= read -r link; do
     
     if [[ $link == *".RDS"* ]]
     then
-        wget -P data_files/tsv $link
+        wget -P data_files/rds $link
     else
-        wget -P data_file/rds $link
+        wget -P data_files/tsv $link
     fi
 
 done < links.txt
